d60cae7a3777d971f66592e03de15abf76359bfb
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-05-16 02:36:01 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useReducer } from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# musicGen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-anynfr)
